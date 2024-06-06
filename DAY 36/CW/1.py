a25bf00dcc75bccbae020a88e2e2a452cab00ae0
Dimitri-3000 <162152694+Dimitri-3000@users.noreply.github.com>
2024-06-06 18:01:02 +0400

    Add files via upload

---
+++ DAY 36/CW/1.py
@@ -0,0 +1,14 @@
+myinfo = {
+    "name" : "dimitri",
+    "LASTNAME" : "ugrekhelidze",
+    "age" : 16
+}
+
+
+for i in myinfo:
+    mylist=[]
+    mylist.append(myinfo.values())
+
+print(mylist)
+
+
