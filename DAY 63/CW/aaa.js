a9fa0b46a71297bb1f8454faa7a77526fc7c2483
Dimitri-3000 <162152694+Dimitri-3000@users.noreply.github.com>
2024-09-08 20:24:03 +0400

    Add files via upload

---
+++ DAY 63/CW/aaa.js
@@ -0,0 +1,116 @@
+
+
+
+
+function a(a){
+    if (a % 2 == 0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+aaa = [1,4,7,8,9,3,4]
+
+function manalfilter (innerfunc, mass){
+    let newarr = []
+    for(let i; i < mass.length - 1; i++){
+        if (innerfunc(mass[i]) == true){
+            newarr.push[mass[i]]
+        }
+        
+
+        return newarr
+
+    }
+}
+
+console.log(manalfilter(a, aaa))
+
+
+//
+
+
+
+console.log(math.sqrt(81));
+console.log(math.PI)
+console.log(math.ceil(6.8))
+console.log(math.floor(-8.7))
+console.log(Math.trunc(-8.7))
+console.log(Math.pow(3, 3))
+console.log(Math.abs(-9))
+console.log(Math.min(1,9,8))
+console.log(Math.max(1,9,8))
+
+
+//
+
+
+function hmm (){
+    let a = new Date()
+    let b = a.getMinutes()
+    if (b == 35){
+        clearInterval()
+    }
+}
+
+setInterval(hmm, 2000)
+
+
+function yo (){
+    console.log("hi")
+
+}
+
+setInterval(yo, 3000)
+
+
+//
+
+const Nnew = new Date();
+
+console.log(Nnew.getMonth());
+console.log(Nnew.getFullYear())
+console.log(Nnew.getDate())
+console.log(Nnew.getDay())
+console.log(Nnew.getHours())
+console.log(Nnew.getMinutes())
+console.log(Nnew.getSeconds())
+console.log(Nnew.getMilliseconds())
+
+console.log("pirveli")
+
+const pirveli = new Date("2022-03-25");
+
+console.log(pirveli.getMonth());
+console.log(pirveli.getFullYear())
+console.log(pirveli.getDate())
+console.log(pirveli.getDay())
+console.log(pirveli.getHours())
+console.log(pirveli.getMinutes())
+console.log(pirveli.getSeconds())
+console.log(pirveli.getMilliseconds())
+
+console.log("meore")
+
+const meore = new Date("October 13, 2014 11:13:00");
+
+console.log(meore.getMonth());
+console.log(meore.getFullYear())
+console.log(meore.getDate())
+console.log(meore.getDay())
+console.log(meore.getHours())
+console.log(meore.getMinutes())
+console.log(meore.getSeconds())
+console.log(meore.getMilliseconds())
+
+
+
+
+
+
+
+
+
+
