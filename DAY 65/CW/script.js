66027d46dda68fa9f302e678d3c66692dd5c65ce
Dimitri-3000 <162152694+Dimitri-3000@users.noreply.github.com>
2024-09-26 23:24:34 +0400

    Add files via upload

---
+++ DAY 65/CW/script.js
@@ -0,0 +1,22 @@
+
+
+
+// document.body.children[0].textContent = "Heloo !"
+// document.body.children[0].style.color  = "red"
+
+// function manualget(id){
+//     const element = document.all
+
+//     for (let i = 0; i < element.length; i++){
+//         if(element[i].id === id){
+//             return element[i]
+//         }
+//     }
+// }
+
+// const L = manualget("L");
+
+// console.log(L);
+
+console.dir(document)
+
