1d035065f4103c798de5246d347bb2b69869f337
Dimitri-3000 <162152694+Dimitri-3000@users.noreply.github.com>
2024-12-08 19:38:55 +0400

    Add files via upload

---
+++ DAY 82/CW/script.js
@@ -0,0 +1,20 @@
+
+let obj = {
+    name: "grigioli",
+    age: "7",
+    height: "1.7m"
+}
+
+for(let key in obj){
+    console.log(obj[key])
+}
+
+let gg = [1,"ori",3,4,true]
+
+for(let a of gg){
+    console.log(a)
+}
+
+for (let i in gg){    
+    console.log(gg[i])
+}
